a23328918671741c7bc028a15d5b4832159b5624
Felizardo <devfelizardo@gmail.com>
2020-12-31 23:51:06 -0300

    finishing authenticating screen with links

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,40 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View, KeyboardAvoidingView, ScrollView, Platform } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import logoImg from "../../assets/logo.png";
 
-import { Container, Title } from "./styles";
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu login</Title>
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : undefined} enabled>
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Button onPress={() => console.log("Deu")}>Entrar</Button>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-      <Button onPress={() => console.log("Deu")}>Entrar</Button>
-    </Container>
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
